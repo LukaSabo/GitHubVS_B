a19e508aec8d7db578b537c816c8ca430dfa8a02
LukaSabo <luka.sabo@virtua-it.si>
2015-01-09 15:06:36 +0100

    init

---
+++ ConsoleApplication9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Display(new List<bool>{true, false, true});
+            Console.ReadKey();
+        }
+
+        static void Display(IEnumerable<bool> argument)
+        {
+            foreach (var a in argument)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
